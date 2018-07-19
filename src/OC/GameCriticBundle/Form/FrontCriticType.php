6aed7092fd96aab03ecc8535a224898475a7bb52
Guillaume BENOIT <gbenoit79@gmail.com>
2018-07-19 22:15:20 +0200

    Design for create critic page

--- src/OC/GameCriticBundle/Form/FrontCriticType.php
+++ src/OC/GameCriticBundle/Form/FrontCriticType.php
@@ -8,7 +8,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class CriticType extends AbstractType
+class FrontCriticType extends AbstractType
 {
     /**
      * {@inheritdoc}
